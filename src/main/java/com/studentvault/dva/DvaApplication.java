25a5c4f563a73e7f81d088dc16836e84363cc8f7
Tomas <tomasdi.dev@gmail.com>
2026-04-26 12:11:00 -0500

    first commit

---
+++ src/main/java/com/studentvault/dva/DvaApplication.java
@@ -0,0 +1,13 @@
+package com.studentvault.dva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DvaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DvaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/studentvault/dva/DvaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studentvault.dva;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DvaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
